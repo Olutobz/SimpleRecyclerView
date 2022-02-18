1f9f12a9578ab6dec4c2e695eaf21b815fd77f29
Onikoyi Damola Olutoba <Damexxey94@gmail.com>
2022-02-18 01:01:27 +0100

    display the movieList from the data source for the recyclerview

--- app/src/main/java/com/olutoba/simplerecyclerview/MainActivity.kt
+++ app/src/main/java/com/olutoba/simplerecyclerview/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.olutoba.simplerecyclerview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.olutoba.simplerecyclerview.adapter.MovieTitleAdapter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val movieList = Datasource(this).getMovieList()
+        val recyclerView: RecyclerView = findViewById(R.id.movie_list)
+        recyclerView.adapter = MovieTitleAdapter(movieList)
     }
 }
