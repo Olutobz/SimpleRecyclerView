2cd7eaf8df44618bfa7edf0888867c71efb73566
Onikoyi Damola Olutoba <Damexxey94@gmail.com>
2022-02-22 01:45:20 +0100

    implement viewmodel and its viewmodel factory

---
+++ app/src/main/java/com/olutoba/simplerecyclerview/movieList/MoviesListViewModel.kt
@@ -0,0 +1,35 @@
+package com.olutoba.simplerecyclerview.movieList
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.olutoba.simplerecyclerview.data.Datasource
+import com.olutoba.simplerecyclerview.data.Movie
+import kotlin.random.Random
+
+class MoviesListViewModel(private val datasource: Datasource) : ViewModel() {
+
+    val moviesLiveData = datasource.getMovieList()
+
+    fun addMovie(movieName: String?) {
+        if (movieName == null) {
+            return
+        }
+        val image = datasource.getRandomMovieImageAsset()
+        val newMovie = Movie(Random.nextLong(), movieName, image)
+        datasource.addMovie(newMovie)
+    }
+}
+
+@Suppress("UNCHECKED_CAST")
+class MoviesListViewModelFactory(private val context: Context) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(MoviesListViewModel::class.java)) {
+            return MoviesListViewModel(
+                datasource = Datasource.getDataSource(context.resources)
+            ) as T
+        } else throw IllegalArgumentException("Unknown ViewModel class")
+    }
+
+}
